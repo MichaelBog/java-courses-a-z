94dc2f315e5fe92726da56b106f2b5c8c54494f0
Michael <mybogomolov@gmail.com>
2016-10-01 23:36:42 +0300

    add 4 methods

--- src/main/java/mbogomolov/Calculate.java
+++ src/main/java/mbogomolov/Calculate.java
@@ -1,5 +1,18 @@
 package mbogomolov;
 public class Calculate {
-public static void main(String[] args){
-System.out.println("Hello World!");}  
-}
+ 	 double result;
+ 	 
+		public void add(double first, double second){
+			result = first + second;
+		}
+		public void substruct(double first, double second){
+			result = first - second;
+		}
+		public void multiple(double first, double second){
+			result = first * second;
+		}
+		
+		public void div(double first, double second){
+			result = first / second;
+		}
+	}
