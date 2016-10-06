2cc41dcf89f4f0beac047fd51c07801f6604edf1
Michael <mybogomolov@gmail.com>
2016-10-07 00:45:48 +0300

    add Triangle

---
+++ src/main/java/ru/mbogomolov/Triangle.java
@@ -0,0 +1,24 @@
+package ru.mbogomolov;
+import ru.mbogomolov.Point;
+
+public class Triangle {
+public Point a;
+public Point b;
+public Point c;
+
+public Triangle(Point a, Point b, Point c) {
+this.a = a;
+this.b = b;
+this.c = c;
+}
+
+public double area() {
+//calculate the triangle area
+if (((a.distanceTo(b)) == 0) || ((b.distanceTo(c)) == 0) || ((c.distanceTo(a)) == 0)){
+	System.out.println("Невозможно построить треугольник");
+} else {
+double halfPerimeter = (((a.distanceTo(b))+(b.distanceTo(c))+(c.distanceTo(a))) / 2); 	
+return Math.sqrt(halfPerimeter*(halfPerimeter - a.distanceTo(b))*(halfPerimeter - b.distanceTo(c))*(halfPerimeter - c.distanceTo(a)));
+}
+}
+}
