0ca5ae76a71bd4ee3ae0fd7344344fa36478ab54
Michael <mybogomolov@gmail.com>
2016-10-07 00:46:58 +0300

    add Point

---
+++ src/main/java/ru/mbogomolov/Point.java
@@ -0,0 +1,16 @@
+package ru.mbogomolov;
+
+public class Point {
+public double x;
+public double y;
+
+public Point(double x, double y) {
+this.x = x;
+this.y = y;
+}
+
+public double distanceTo(Point point) {
+//calculate distance between two points
+return Math.sqrt(Math.pow(point.y - this.y, 2) + Math.pow(point.x - this.x, 2));
+}
+}
