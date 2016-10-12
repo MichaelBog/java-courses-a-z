783dbf8ba2e24c78a280a75af5bf24d741f7fa2c
Michael <mybogomolov@gmail.com>
2016-10-12 23:38:46 +0300

    new tests

--- chapter_001/src/test/java/ru/mbogomolov/AreaTest.java
+++ chapter_001/src/test/java/ru/mbogomolov/AreaTest.java
@@ -1,17 +1,20 @@
 package ru.mbogomolov;
 
+
 import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
 import org.junit.Test;
 
 public class AreaTest{
 	@Test
-	public void WhenSetDistanceFromPointsThenRecieveTriangleArea(){
-		Triangle triangle = new Triangle(1,1; 2,2; 3,3);
-		final double area = triangle.area;
-		assertThat(area, is(3.1196));
+	public void WhenSetDistanceFromPointsThenRecieveTriangleArea(){ 
+    Point a = new Point(1,1);    
+    Point b = new Point(2,2);
+    Point c = new Point(3,3);
+		Triangle triangle = new Triangle(a,b,c);
+		final double result = triangle.area();
+		assertThat(result, is(3.1196));
 	}
 	
-	}
-
 }
+
--- chapter_001/src/test/java/ru/mbogomolov/PointTest.java
+++ chapter_001/src/test/java/ru/mbogomolov/PointTest.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 public class PointTest{
 	@Test
 	public void whenSetPointThenReciveDistanceFromAtoB() {
-		Point point a = new Point(1,1);
-		Point point b = new Point(2,2);
+		Point a = new Point(1,1);
+		Point b = new Point(2,2);
 		final double distance = a.distanceTo(b);
 		assertThat(distance, is(2));
 	}
