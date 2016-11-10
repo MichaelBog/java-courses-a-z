40e0e0c57dc78667be2f46dbfb78494629a5d354
Michael <mybogomolov@gmail.com>
2016-11-10 23:00:56 +0300

    add bubbleSort

--- chapter_001/src/test/java/ru/mbogomolov/BubbleSortTest.java
+++ chapter_001/src/test/java/ru/mbogomolov/BubbleSortTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 public class BubbleSortTest {
 	@Test
     public void whenRecieveArrayThenSort() {
-    	//BubbleSort array = new BubbleSort(new int[] {1,2,7,8});
     	int[] array = new int[] {1,2,7,8};
     	int[] result = BubbleSort.bubbleSort(array);
     	assertThat(result, is(new int[] {8,7,2,1}));
