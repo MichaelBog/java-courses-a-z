6566094a742f791a8677d5a073820828bbb89914
Michael <mybogomolov@gmail.com>
2016-12-09 22:30:16 +0300

    remove "for", add "while" loop

--- chapter_001/src/main/java/ru/mbogomolov/FindSubString.java
+++ chapter_001/src/main/java/ru/mbogomolov/FindSubString.java
@@ -2,7 +2,7 @@ package ru.mbogomolov;
 /**
  * Find substring from original.
  * @author Michael Bogomolov (mybogomolov@gmail.com)
- * @version
+ * @version 2
 */
 
 public class FindSubString {
@@ -28,14 +28,16 @@ public FindSubString(String origin, String sub) {
     * @return boolean
     */
 	public boolean findSubString() {
-		int j;
-		int i;
+		int j = 0;
+		int i = 0;
 		char[] originArr = this.origin.toCharArray();
 		char[] subArr = this.sub.toCharArray();
-		for (i = 0, j = 0; i < originArr.length && j < subArr.length; i++) {
-			if (originArr[i] == subArr[j]) {
+		while(i < originArr.length && j < subArr.length) {
+		if (originArr[i] == subArr[j]) {
                 j++;
+                i++;
             } else {
+				i++;
 				i -= j;
 				j = 0;
 			}
